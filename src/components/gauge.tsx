d1e8ffece0188f219110f383b416fec32b99da6b
M Bentar Raya T <bentarraya01@gmail.com>
2025-12-15 15:20:37 +0000

    masih terlihat besar dan berdekatan dengan gauge, jadi perkecil lagi ata

--- src/components/gauge.tsx
+++ src/components/gauge.tsx
@@ -45,7 +45,7 @@ const Gauge: React.FC<GaugeProps> = ({ value, maxValue, unit }) => {
             y="50"
             textAnchor="middle"
             dominantBaseline="text-bottom"
-            className="fill-foreground text-xl font-bold font-mono"
+            className="fill-foreground text-lg font-medium font-mono"
           >
             {safeValue.toFixed(1)}
              <tspan dy="-0.2em" className="text-base font-medium">{unit}</tspan>
